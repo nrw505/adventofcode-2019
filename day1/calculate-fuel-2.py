700e33169099fdb7393031a814a8267f7c354831
Nigel Williams <nigel.williams@yubico.com>
2019-12-02 16:38:23 +1100

    Cleanup

--- day1/calculate-fuel-2.py
+++ day1/calculate-fuel-2.py
@@ -8,11 +8,11 @@ def fuel_for_mass(mass):
         return 0;
     fuel_for_fuel = fuel_for_mass(fuel_required)
     return fuel_required + fuel_for_fuel
-    
+
 
 total_fuel_required = 0
 for line in sys.stdin:
     mass = int(line)
     total_fuel_required += fuel_for_mass(mass)
-    
+
 print(f"Total fuel required: {total_fuel_required}")
--- day1/calculate-fuel.py
+++ day1/calculate-fuel.py
@@ -5,11 +5,11 @@ import math
 def fuel_for_mass(mass):
     fuel_required = math.floor(mass / 3) - 2
     return fuel_required
-    
+
 
 total_fuel_required = 0
 for line in sys.stdin:
     mass = int(line)
     total_fuel_required += fuel_for_mass(mass)
-    
+
 print(f"Total fuel required: {total_fuel_required}")
