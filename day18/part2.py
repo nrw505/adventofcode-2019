01ed0af21b750ec2dba18f78a302364b1e8cdb3b
Nigel Williams <nigel.williams@yubico.com>
2019-12-20 21:54:07 +1100

    Finally, day 18

---
+++ day18/part2.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+import re
+import operator
+from collections import deque
+
+from grid import Grid
+
+
+infile = open(sys.argv[1])
+grid = Grid()
+
+starts = (None, None, None, None)
+start = None
+keys = {}
+doors = {}
+
+def adjacent(pos):
+    return [
+        x
+        for x in [
+            (pos[0] + 1, pos[1]),
+            (pos[0] - 1, pos[1]),
+            (pos[0], pos[1] + 1),
+            (pos[0], pos[1] - 1),
+        ]
+        if x in grid
+    ]
+
+
+y = 0
+for line in infile:
+    line = line.strip()
+    x = 0
+    for c in line:
+        grid[(x, y)] = c
+        if c == "@":
+            start = (x, y)
+        if "a" <= c <= "z":
+            keys[c] = (x, y)
+        if "A" <= c <= "Z":
+            doors[c.lower()] = (x, y)
+        x += 1
+    y += 1
+
+for pos in adjacent(start):
+    grid[pos] = "#"
+grid[start] = "#"
+
+starts = (
+    (start[0] + 1, start[1] + 1),
+    (start[0] + 1, start[1] - 1),
+    (start[0] - 1, start[1] + 1),
+    (start[0] - 1, start[1] - 1),
+)
+
+
+def find_accessible_keys(pos, held_keys, distance=1, already_done=None):
+    paths = {}
+    blocking_doors = {}
+
+    done = set()
+    next_iteration = adjacent(pos)
+    if already_done:
+        done = already_done.copy()
+        next_iteration = [doors[x] for x in doors if doors[x] in done]
+        for x in next_iteration:
+            done.remove(x)
+
+    while next_iteration:
+        this_iteration = next_iteration
+        next_iteration = []
+        for check in this_iteration:
+            c = grid[check]
+            done.add(check)
+            if c == "#":
+                continue
+            if c.isupper() and c.lower() not in held_keys:
+                blocking_doors[c.lower()] = distance
+                continue
+            if (
+                c.islower()
+                and c not in paths
+                and c not in held_keys
+            ):
+                paths[c] = distance
+            next_iteration.extend([x for x in adjacent(check) if x not in done])
+        distance += 1
+
+    return (paths, blocking_doors, done)
+
+
+all_paths = {}
+def generate_all_paths(pos, held_keys='', distance=1, done=None, already_blocking=''):
+    if pos not in all_paths:
+        all_paths[pos] = {}
+
+    if not done:
+        done = set()
+
+    (paths, blocking_doors, done) = find_accessible_keys(pos, held_keys, distance, done)
+    if not paths and not blocking_doors:
+        return
+
+    for path in paths:
+        all_paths[pos][path] = { "distance": paths[path], "doors": held_keys }
+
+    for door in blocking_doors:
+        if door not in already_blocking:
+            generate_all_paths(pos, held_keys + door, blocking_doors[door], done, blocking_doors.keys())
+
+
+def accessible_keys(pos_set, held_keys):
+    accessible = {}
+
+    for pos in pos_set:
+        for key in all_paths[pos]:
+            if key not in held_keys and set(all_paths[pos][key]["doors"]).issubset(set(held_keys)):
+                accessible[key] = all_paths[pos][key]["distance"]
+    return accessible
+
+
+def leaf_length(pos_set, path):
+    length = 0
+    held = ""
+    for k in path:
+        acc = accessible_keys(pos_set, held)
+        length += acc[k]
+        pos = keys[k]
+        held += k
+    return length
+
+
+leaf_lengths = {}
+
+explore_from_cache = {}
+
+def next_pos_set(pos_set, key):
+    nextset = [None] * 4
+    for i in range(4):
+        if key in all_paths[pos_set[i]]:
+            nextset[i] = keys[key]
+        else:
+            nextset[i] = pos_set[i]
+    return tuple(nextset)
+
+
+def explore_from(pos_set, path):
+    held = "".join([x for x in sorted(path)])
+    cache_key = (pos_set, held)
+    if cache_key in explore_from_cache:
+        return explore_from_cache[cache_key]
+
+    options = accessible_keys(pos_set, path)
+    if not options:
+        return (0, "")
+
+    dists = {}
+    paths = {}
+    for option in options:
+        nextset = next_pos_set(pos_set, option)
+        (d, p) = explore_from(nextset, path + option)
+        dists[option] = d + options[option]
+        paths[option] = option + p
+
+    shortest = min(dists.items(), key=operator.itemgetter(1))[0]
+    ret = (dists[shortest], paths[shortest])
+
+    explore_from_cache[cache_key] = ret
+    return ret
+
+for start in starts:
+    generate_all_paths(start)
+for key in keys:
+    generate_all_paths(keys[key])
+
+(m, p) = explore_from(starts, "")
+print(f"Length: {m}")
+print(f"Path: {p}")
